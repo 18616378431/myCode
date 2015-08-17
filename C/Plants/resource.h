76e3e0d3ad0f6a85ebc0d0f152481fa7d419e7ca
18616378431 <18616378431@163.com>
2015-08-17 20:55:35 +0800

    Create resource.h

---
+++ C/Plants/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by plants1.rc
+//
+#define IDB_BITMAP1                     101
+#define IDI_ICON1                       102
+#define IDB_BITMAP2                     104
+#define IDR_MENU1                       105
+#define IDB_BITMAP3                     106
+#define IDB_BITMAP4                     107
+#define ID_OPEN                         40004
+#define ID_EXIT                         40005
+#define ID_ABOUT                        40006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
