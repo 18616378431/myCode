c5b4d5dd1ba9fc753e2a11e793ba0174ec60b3bb
18616378431 <18616378431@163.com>
2015-08-17 20:57:41 +0800

    Create resource.h

---
+++ CPP/TianTianKuPao/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 天天酷跑.rc
+//
+#define IDC_CURSOR1                     101
+#define IDI_ICON1                       102
+#define IDR_MENU1                       103
+#define IDB_BITMAP1                     104
+#define IDB_BITMAP2                     105
+#define IDB_BITMAP3                     106
+#define IDB_BITMAP4                     107
+#define IDB_BITMAP5                     108
+#define IDB_BITMAP6                     109
+#define IDB_BITMAP7                     110
+#define IDB_BITMAP8                     111
+#define IDB_BITMAP9                     112
+#define IDB_BITMAP10                    113
+#define IDB_BITMAP11                    114
+#define IDB_BITMAP12                    115
+#define IDB_BITMAP13                    116
+#define IDB_BITMAP14                    117
+#define IDB_BITMAP15                    118
+#define ID_BEGIN                        40002
+#define ID_EXIT                         40004
+#define ID_ABOUT                        40006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        119
+#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
