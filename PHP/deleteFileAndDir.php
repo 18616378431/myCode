8ba9a14a5966ef086d170e25a9a6a0362692408b
18616378431 <18616378431@163.com>
2015-08-18 13:07:06 +0800

    Update deleteFileAndDir.php

--- PHP/deleteFileAndDir.php
+++ PHP/deleteFileAndDir.php
@@ -38,3 +38,60 @@
 */
 	
 ?>
+
+//只删除文件，不删除空文件夹
+function  rdir($dir){
+        $files = array();
+        $dirPath = realpath($dir);
+        $fileNames = scandir($dir);
+//        var_dump($dirPath);
+//        var_dump($fileNames);
+        foreach($fileNames as $fileName){
+            if($fileName == '.' || $fileName == '..'){
+                continue;
+            }
+            $file = $dirPath.DIRECTORY_SEPARATOR.$fileName;
+            $files[] = $file;
+            if(is_dir($file)){
+                //echo "<span style='color: red;'>  dir</span><hr />";
+                rdir($file);
+            }else{
+                $f = explode(".", $fileName);
+                $arr = array('c','h');
+                if(!in_array($f[1],$arr)){
+                    //echo $fileName."::".$file."<hr />";
+                    unlink($file);
+                }
+            }
+        }
+//        var_dump($files);
+        return $files;
+}
+rdir($dir);
+
+//删除文件以及空文件夹，过滤掉目标类型文件
+function check($path){
+    if(file_exists($path)){
+        //echo dirname($path).'<br />';
+        $dh = opendir($path);
+        while($item = readdir($dh)){
+            if('.' == $item || '..' == $item){
+                continue;
+            }else if(is_dir($path.'/'.$item)){
+                check($path.'/'.$item);
+            }else{
+                $arr = array('c','h');
+                $f = explode(".", $item);
+                if(!in_array($f[1],$arr)){
+                    //echo $fileName."::".$file."<hr />";
+                    unlink($path.'/'.$item);//
+                }
+            }
+        }
+        closedir($dh);
+        if(@rmdir($path.'/'.$item)){
+            echo "删除文件夹成功".$path."<br />";
+        }
+    }
+}
+check("./destination");
