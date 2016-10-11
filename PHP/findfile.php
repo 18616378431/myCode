505559e08a580fe17685bf18585dfe395b0631f2
MonikerSay <18616378431@163.com>
2016-10-11 14:23:11 +0800

    查询目录下的文件，搜索目录下得目标文件
    查询目录下的文件，搜索目录下得目标文件

--- PHP/findfile.php
+++ PHP/findfile.php
@@ -2,10 +2,10 @@
 
 class findFile{
     /**
-    查询目录下的文件
-    $dir 目录全路径
-    $filterString  查询文件名的字符串，如果为空的话，返回目录所有文件
-    return:符合条件的文件
+    * 查询目录下的文件
+    * $dir 目录全路径
+    * $filterString  查询文件名的字符串，如果为空的话，返回目录所有文件
+    * return:符合条件的文件
      */
     public function getFilenames( $dir,$filterString='',$scanChildDir = false )
     {
