18568b0437d66c5d8887f2fca47ab9ef46f7e66d
MonikerSay <18616378431@163.com>
2016-11-21 15:31:35 +0800

    计算出PHP当前内存大小,根据字节计算出占用内存
    计算出PHP当前内存大小,根据字节计算出占用内存

--- PHP/memory.php
+++ PHP/memory.php
@@ -1,15 +1,13 @@
 <?php
-$start = time();
-$startM = memory_get_usage();//此处程序开始时分配的内存
-
+/**
+ * Created by PhpStorm.
+ * User: jiankang.wang
+ * Date: 2016/11/21
+ * Time: 15:30
+ */
 //计算出PHP当前内存大小,根据字节计算出占用内存
 function convert($size)
 {
     $unit=array('b','kb','mb','gb','tb','pb');
     return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[(int)$i];
 }
-
-$endM = memory_get_usage();//此处程序结束时分配的内存
-$end = time();
-占用的内存为".convert($endM-$startM)
-?>
