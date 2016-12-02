7446569add46070505d34e963d2756b17817f793
MonikerSay <18616378431@163.com>
2016-12-02 10:19:16 +0800

    多进程 执行文件

--- PHP/multipart-process/log.php
+++ PHP/multipart-process/log.php
@@ -2,6 +2,13 @@
 echo "PID:".getmypid()."\n";
 echo "开始时间:".$start = date('Y-m-d H:i:s')."\n";
 
+$mydir = 'tmp/0305';
+if(!is_dir($mydir)){
+	if(!mkdir($mydir,0777,true)){
+        echo "文件夹创建失败!\n";
+    }
+}
+
 sleep(2);
 
 echo "结束时间:".$end = date('Y-m-d H:i:s')."\n";
