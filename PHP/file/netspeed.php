0b982e53c4d6e8416df5cec9acac119288321448
MonikerSay <18616378431@163.com>
2017-01-04 16:59:45 +0800

    通过输出文件速度计算网速
    通过输出文件速度计算网速

---
+++ PHP/file/netspeed.php
@@ -0,0 +1,18 @@
+<?php
+//创建文件
+//$fp = fopen('test.txt','w');
+//
+//for($i = 0;$i < 170400;$i++){
+//    fwrite($fp,"test\n");
+//}
+//fclose($fp);
+
+$data = file_get_contents("test.txt");
+$start = microtime(true);
+$filesize = filesize("test.txt")/2014;//计算结果为kb
+echo "<!--".$data."-->";
+$end = microtime(true);
+$duration = ($end-$start);
+$speed = round($filesize/$duration,2);
+echo "你的网速:$speed kb/秒\n";
+?>
