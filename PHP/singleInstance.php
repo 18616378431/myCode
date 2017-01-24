5d65321c6c1961e50d51758c7f8b08d106239e69
MonikerSay <18616378431@163.com>
2017-01-24 16:54:34 +0800

    php单例模式
    php单例模式

--- PHP/singleInstance.php
+++ PHP/singleInstance.php
@@ -1,16 +1,20 @@
 <?php
 class User {
-    protected $user_name;
-    public static $instance;
-    
-    private function __construct() {}  // 私有构造函数，避免多次创建对象，导致对象的不唯一
-    private function clone() {}  // 避免克隆（保证单例对象的唯一性）
-
-    public static function getInstance() { 
-        if (null === self::$instance) {
-            self::$instance = new self(); 
+    private static $_instance;
+    private function __construct()
+    {
+        echo "获得一个新对象";
+    }
+    private function __clone()
+    {
+        // 覆盖clone()方法，禁止克隆
+    }
+    public static function getInstance()
+    {
+        if (! (self::$_instance instanceof self)) {
+            self::$_instance = new self();
         }
-        return self::$instance;
+        return self::$_instance;
     }
 }
 ?>
